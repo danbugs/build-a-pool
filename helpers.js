ee42b879a7b197a42381fe7735f4ac2983a44fa2
Dan Chiarlone <danilochiarlone@gmail.com>
2020-01-18 23:02:13 -0800

    test index.html

--- helpers.js
+++ helpers.js
@@ -13,13 +13,3 @@ Handlebars.registerHelper('getObjectName', function(object) {
 Handlebars.registerHelper('getObject', function(object) {
         return JSON.stringify(object);
         }); 
-
-
-
-fetch('data.json')
-    .then(response => response.json())
-    .then(json => {
-        var main = Handlebars.compile($("#main").html());
-        Handlebars.registerPartial("courses", $("#courses").html());
-        $("body").append(main(json));
-    });
