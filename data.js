5edaa9e2ab144ea00cfefeb9f07bdc02940c7356
Dan Chiarlone <danilochiarlone@gmail.com>
2020-01-18 23:49:49 -0800

    cleanup

--- data.js
+++
@@ -1,7 +0,0 @@
-fetch('data.json')
-    .then(response => response.json())
-    .then(json => {
-        var main = Handlebars.compile($("#main").html());
-        Handlebars.registerPartial("courses", $("#courses").html());
-        $("body").append(main(json));
-    });
